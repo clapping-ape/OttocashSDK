838b4ef8b734a13fc29121c96555942efad91e7d
Nur Choirudin <nurchoi@clappingape.com>
2019-05-17 01:44:12 +0700

    change name

---
+++ OttocashSDKDemo/Pods/Target Support Files/OttocashSDK/OttocashSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_OttocashSDK : NSObject
+@end
+@implementation PodsDummy_OttocashSDK
+@end
--- OttocashSDKDemo/Pods/Target Support Files/Pods-OttocashSDKDemo/Pods-OttocashSDKDemo-frameworks.sh
+++ OttocashSDKDemo/Pods/Target Support Files/Pods-OttocashSDKDemo/Pods-OttocashSDKDemo-frameworks.sh
@@ -154,9 +154,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${PODS_ROOT}/OttocashSDK/OTTOCashSDK.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/OttocashSDK/OttocashSDK.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${PODS_ROOT}/OttocashSDK/OTTOCashSDK.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/OttocashSDK/OttocashSDK.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
