1287822800de84ec1d36d477dd01fc0cfb63af13
renandus <renandus@clappingape.com>
2019-07-29 17:45:06 +0700

    update demo

--- OttocashSDKDemo/OttocashSDKDemo/ViewController.swift
+++ OttocashSDKDemo/OttocashSDKDemo/ViewController.swift
@@ -18,10 +18,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         otto = OTTOCashSDKManager.shared
-        otto?.setupClient(id: "f04e0a8a7a129b42d3089ebb68bc3c09671c0f23d1ee2a25a996883823844d60", secret: "a136e35b69881a9496a14d2652976c72054cf18ae57ffb7461263a41bf3f2866")
-        otto?.setDebugMode(true)
+        otto?.setupClient(id: "CLIENT_ID", secret: "CLIENT_SECRET")
         let userLocation:CLLocation = CLLocation(latitude: 0.0, longitude: 0.0)
-        otto?.userIdentity(name: "iLotte", location: userLocation.coordinate, phone: "085659791134")
+        otto?.userIdentity(name: "[APP_NAME]", location: userLocation.coordinate, phone: "[PHONE NUMBER]")
     }
 
     override func viewWillAppear(_ animated: Bool) {
