d0b9a64eea43b3e611ed85e8d7bfe9683cc370e0
renandus <renandus@clappingape.com>
2019-08-08 16:37:37 +0700

    setup aggregate

--- OttocashSDKDemo/OttocashSDKDemo/AppDelegate.swift
+++ OttocashSDKDemo/OttocashSDKDemo/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Fabric.with([Crashlytics.self])
+
         // Override point for customization after application launch.
         return true
     }
